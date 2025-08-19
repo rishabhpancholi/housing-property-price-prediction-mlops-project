03f497f3e5f7b448e862480eded4bfca21c32a7f
Rishabh Pancholi <rp23@iitbbs.ac.in>
2025-08-19 21:43:04 +0530

    Modified data ingestion config class

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -17,11 +17,19 @@ class DataIngestionConfig:
     This class stores all the config variables for data ingestion imported from constants.
 
     """
-    aws_access_key_id: str = os.getenv("AWS_ACCESS_KEY_ID")
+    bucket_name:str = os.getenv("BUCKET_NAME")
+    endpoint_url:str = os.getenv("ENDPOINT_URL")
+    region_name:str = os.getenv("REGION_NAME")
+    raw_data_key:str = os.getenv("RAW_DATA_KEY")
+    interim_train_data_key:str = os.getenv("INTERIM_TRAIN_DATA_KEY")
+    interim_val_data_key:str = os.getenv("INTERIM_VAL_DATA_KEY")
+    interim_test_data_key:str = os.getenv("INTERIM_TEST_DATA_KEY")
+    aws_access_key_id:str = os.getenv("AWS_ACCESS_KEY_ID")
     aws_secret_access_key:str = os.getenv("AWS_SECRET_ACCESS_KEY")
-    raw_data_s3_path: str = os.getenv("RAW_DATA_S3_PATH")
-    test_ratio: float = params["train_test_split"]["TEST_RATIO"]
-    val_ratio: float = params["train_test_split"]["VAL_RATIO"]
+    test_ratio:float = params["split"]["TEST_RATIO"]
+    val_ratio:float = params["split"]["VAL_RATIO"]
+    test_random_state:int = params["split"]["TEST_RANDOM_STATE"]
+    val_random_state:int = params["split"]["VAL_RANDOM_STATE"]
 
 # Example usage
 if __name__ == "__main__":
