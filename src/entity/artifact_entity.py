f6ec405b4ba411d070b18dbb8b69387b4487ab60
Rishabh Pancholi <rp23@iitbbs.ac.in>
2025-08-23 10:29:29 +0530

    Updated artifact_entity.py file

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -11,6 +11,15 @@ load_dotenv()
 with open("params.yaml","r") as f:
     params = yaml.safe_load(f)
 
+# Creating an ETLPipelineArtifact class
+@dataclass
+class ETLPipelineArtifact:
+    """
+    This class stores all the artifact variables after ETL pipeline completion.
+
+    """
+    raw_data_key:str = os.getenv("RAW_DATA_KEY")
+
 # Creating a DataIngestionArtifact class
 @dataclass
 class DataIngestionArtifact:
@@ -18,7 +27,7 @@ class DataIngestionArtifact:
     This class stores all the artifact variables after data ingestion completion.
 
     """
-    interim_train_data:str = os.getenv("INTERIM_TRAIN_DATA_KEY")
+    interim_train_data_key:str = os.getenv("INTERIM_TRAIN_DATA_KEY")
     interim_val_data_key:str = os.getenv("INTERIM_VAL_DATA_KEY")
     interim_test_data_key:str = os.getenv("INTERIM_TEST_DATA_KEY")
 
@@ -29,10 +38,25 @@ class DataValidationArtifact(DataIngestionArtifact):
     This class stores all the artifact variables after data validation completion.
 
     """
+# Creating a DataTransformationArtifact class
+@dataclass
+class DataTransformationArtifact:
+    """
+    This class stores all the artifact variables after data transformation completion.
+
+    """
+    preprocessed_train_data_key:str = os.getenv("PREPROCESSED_TRAIN_DATA_KEY")  
+    preprocessed_val_data_key:str = os.getenv("PREPROCESSED_VAL_DATA_KEY")
+    preprocessed_test_data_key:str = os.getenv("PREPROCESSED_TEST_DATA_KEY")
+
 
 # Example usage
 if __name__ == "__main__":
+    etl = ETLPipelineArtifact()
     dia = DataIngestionArtifact()
     dva = DataValidationArtifact()
+    dta = DataTransformationArtifact()
+    print(etl)
     print(dia)
     print(dva)
+    print(dta)
