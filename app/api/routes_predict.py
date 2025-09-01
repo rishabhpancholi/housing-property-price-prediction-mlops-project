cb3e1766abdbcf188ea1e8717652ca0deffd15b8
Rishabh Pancholi <rp23@iitbbs.ac.in>
2025-09-01 11:00:35 +0530

    Added prediction route

--- app/api/routes_predict.py
+++ app/api/routes_predict.py
@@ -3,16 +3,16 @@ from app.model.schema import InputSchema
 from app.services.model_service import get_prediction
 
 # Creating an APIRouter instance
-router = APIRouter()
+predict_router = APIRouter()
 
 # Creating a prediction route
-@router.post("/predict")
+@predict_router.post("/predict")
 async def predict(data: InputSchema)-> dict:
     """
     Returns predictions for the given data
 
     """
-    prediction  = get_prediction(data.model_dump())
+    prediction  = await get_prediction(data.model_dump())
     return {"prediction": f'{prediction:,.2f}'} 
 
 
