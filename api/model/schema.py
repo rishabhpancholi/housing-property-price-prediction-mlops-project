31b6f7a8dff54b3005ac5e4a2ea835fec6707386
Rishabh Pancholi <rp23@iitbbs.ac.in>
2025-09-01 14:48:05 +0530

    Updated schema.py file

--- api/model/schema.py
+++ api/model/schema.py
@@ -1,6 +1,6 @@
 from typing import Optional,Literal
 from pydantic import BaseModel,Field,model_validator
-from app.core.enums import LocationEnum,FacingEnum
+from api.core.enums import LocationEnum,FacingEnum
 
 # Creating a Pydantic model for input data
 class InputSchema(BaseModel):
@@ -8,7 +8,10 @@ class InputSchema(BaseModel):
    This class contains the detailed schema for a particular row in the data.
 
    """
-   location:LocationEnum = Field(description = "City in which the house is located")
+   location:LocationEnum = Field(description = """
+   City in which the house is located.
+   Please provide the city name in small letters.""", 
+   example = "mumbai")
    num_bhk:int = Field(..., ge = 0, description = "Number of bhks in the house")
    transaction:Literal["Resale", "New Property", "Other", "Rent/Lease"] = Field(description = "Type of transaction")
    furnishing:Optional[Literal["Unfurnished", "Furnished", "Semi-Furnished"]] = Field(description = "Furnishing status of the house", default = None)
@@ -37,3 +40,20 @@ class InputSchema(BaseModel):
    ownership:Optional[Literal["Freehold", "Leasehold", "Co-operative Society", "Power Of Attorney"]] = Field(description = "Ownership status of the house", default = None)
    parking_cover:Optional[Literal["Open", "Covered"]] = Field(description = "Whether the house has open or covered parking", default = None)
    parking_spots:Optional[int] = Field(description = "Number of parking spots in the house", default = None)
+
+   @model_validator(mode = "after")
+   def validate_string_inputs(cls, model):
+      """
+      Method to validate that all string inputs are in appropriate casing and no leading or trailing spaces
+
+      """
+      model.location = model.location.strip().lower()
+      model.transaction = model.transaction.strip().title()
+      model.furnishing = model.furnishing.strip().title() if model.furnishing is not None else None
+      model.facing = model.facing.strip().title() if model.facing is not None else None
+      if model.facing == "South - West":
+        model.facing = "South -West"
+      model.ownership = model.ownership.strip().title() if model.ownership is not None else None
+      model.parking_cover = model.parking_cover.strip().title() if model.parking_cover is not None else None
+
+      return model
